469778f35a534643e9937a029b42dd2ee715272c
yrong <yrong1997@gmail.com>
2018-06-01 18:31:47 +0800

    license目录和文件名根据环境变量加载(0.5h)

--- generate.js
+++ generate.js
@@ -5,10 +5,8 @@ const path = require('path')
 const fs = require('fs')
 
 let generateLicense = function() {
-    let licenseFilePath = process.env.licenseFilePath||path.resolve('./license.json')
-    let result = crypto.encrypt(fs.readFileSync(licenseFilePath, 'utf8'))
-    var package = require(path.resolve('./package.json'))
-    licenseFilePath = path.resolve(licenseFilePath,`../${package.name}.lic`)
+    let result = crypto.encrypt(fs.readFileSync(path.resolve('./license.json'), 'utf8'))
+    let licenseFilePath = process.env.LICENSE_PATH + '/' + process.env.NODE_NAME + '.lic'
     fs.writeFileSync(licenseFilePath,result)
     console.log('license file generated in:'+licenseFilePath)
     return result
@@ -17,4 +15,3 @@ let generateLicense = function() {
 generateLicense()
 
 module.exports = generateLicense
-
